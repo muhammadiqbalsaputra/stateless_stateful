44f8b44f353c46fdf5af8f53efda3a6b421828e8
muhammadiqbalsaputra <muhammad.iqbal.sap@gmail.com>
2025-10-20 09:21:06 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/stateles_stateful/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stateles_stateful
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+// import 'dart:ffi';
+// import 'package:flutter/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int nilai = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter")),
+      body: Center(
+        child: Column(
+          children: [
+            Text("$nilai"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    nilai--;
+                    print(nilai);
+                    setState(() {});
+                  },
+                  label: Text("Kurang"),
+                  icon: Icon(Icons.remove),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    nilai++;
+                    print(nilai);
+                    setState(() {});
+                  },
+                  label: Text("Tambah"),
+                  icon: Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text("Dialog")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class _MyAppState extends State<MyApp> {
+//   // const MyApp({super.key});
+//   int nilai = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(home: Scaffold());
+//   }
+// }
